bdbb641095498bf9062941b59d077ed3656c8935
zachhildreth <zachariahildreth@gmail.com>
2019-12-06 21:20:55 -0800

    General Update
    General Update

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -222,7 +222,7 @@
 #define SOCKET_END_STRING "|END|" // End string when sending data between nodes, to signal the end of sending data
 
 // XCASH DPOPS
-#define BLOCK_VERIFIERS_AMOUNT 18 // The amount of block verifiers in a round
+#define BLOCK_VERIFIERS_AMOUNT 20 // The amount of block verifiers in a round
 #define BLOCK_VERIFIERS_VALID_AMOUNT 12 // The amount of block verifiers that need to vote true for the part of the round to be valid.
 #define BLOCK_VERIFIERS_VALID_AMOUNT_PERCENTAGE 0.67 // The amount of block verifiers in a percentage that need to vote true for the part of the round to be valid.
 
