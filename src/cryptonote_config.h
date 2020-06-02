f5e5694c8de4cb43bf6b58927fec2e48fca4aac4
zachhildreth <zachariahildreth@gmail.com>
2020-06-02 07:12:47 +0000

    test: test
    test

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -246,8 +246,8 @@ full_addrs.insert(SEED_NODE_5);
 
 // XCASH DPOPS
 #define BLOCK_VERIFIERS_TOTAL_AMOUNT 100 // The total amount of block verifiers
-#define BLOCK_VERIFIERS_AMOUNT 34 // The amount of block verifiers in a round
-#define BLOCK_VERIFIERS_VALID_AMOUNT 23 // The amount of block verifiers that need to vote true for the part of the round to be valid.
+#define BLOCK_VERIFIERS_AMOUNT 26 // The amount of block verifiers in a round
+#define BLOCK_VERIFIERS_VALID_AMOUNT 18 // The amount of block verifiers that need to vote true for the part of the round to be valid.
 #define BLOCK_VERIFIERS_VALID_AMOUNT_NETWORK_DATA_NODE 18 // The amount of block verifiers that need to vote true with at least 3 of the network data nodes for the part of the round to be valid.
 #define BLOCK_VERIFIERS_VALID_AMOUNT_PERCENTAGE 0.67 // The amount of block verifiers in a percentage that need to vote true for the part of the round to be valid.
 
