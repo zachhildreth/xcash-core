616a9984ef27f389fb36032eab07c0824923bef0
zachhildreth <zachariahildreth@gmail.com>
2020-06-13 05:39:52 +0000

    feat: Added update_smart_contracts_fee CLI function
    Added update_smart_contracts_fee CLI function.

--- src/simplewallet/simplewallet.h
+++ src/simplewallet/simplewallet.h
@@ -230,7 +230,6 @@ namespace cryptonote
     bool delegate_recover(const std::vector<std::string>& args);
     bool vote_status(const std::vector<std::string>& args);
     bool revote(const std::vector<std::string>& args);
-    bool get_smart_contracts_fee(const std::vector<std::string>& args);
 
     uint64_t get_daemon_blockchain_height(std::string& err);
     bool try_connect_to_daemon(bool silent = false, uint32_t* version = nullptr);
@@ -366,3 +365,4 @@ namespace cryptonote
     uint32_t m_current_subaddress_account;
   };
 }
+
