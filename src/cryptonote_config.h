a2f311258a8a1e0cd821817794a4539a75294512
zachhildreth <zachariahildreth@gmail.com>
2020-06-25 01:21:37 +0000

    fix: Updates for beta
    Updates for beta.

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -218,7 +218,7 @@ full_addrs.insert(SEED_NODE_5);
 // Blockchain
 #define HF_VERSION_PROOF_OF_STAKE 13
 //#define HF_BLOCK_HEIGHT_PROOF_OF_STAKE 521850 // The first block of the X-CASH proof of stake
-#define HF_BLOCK_HEIGHT_PROOF_OF_STAKE 600000 // The first block of the X-CASH proof of stake
+#define HF_BLOCK_HEIGHT_PROOF_OF_STAKE 640000 // The first block of the X-CASH proof of stake
 #define BLOCK_TIME 5 // the block time in minutes
 #define BLOCKS_PER_DAY_FIVE_MINUTE_BLOCK_TIME 288 // The blocks per day with a 5 minute block time
 #define MAXIMUM_RESERVE_BYTES_LEGNTH 4096 // The maximum size in bytes for the reserve bytes in the block header
@@ -246,9 +246,9 @@ full_addrs.insert(SEED_NODE_5);
 
 // XCASH DPOPS
 #define BLOCK_VERIFIERS_TOTAL_AMOUNT 100 // The total amount of block verifiers
-#define BLOCK_VERIFIERS_AMOUNT 26 // The amount of block verifiers in a round
-#define BLOCK_VERIFIERS_VALID_AMOUNT 20 // The amount of block verifiers that need to vote true for the part of the round to be valid.
-#define BLOCK_VERIFIERS_VALID_AMOUNT_NETWORK_DATA_NODE 20 // The amount of block verifiers that need to vote true with at least 3 of the network data nodes for the part of the round to be valid.
+#define BLOCK_VERIFIERS_AMOUNT 50 // The amount of block verifiers in a round
+#define BLOCK_VERIFIERS_VALID_AMOUNT 34 // The amount of block verifiers that need to vote true for the part of the round to be valid.
+#define BLOCK_VERIFIERS_VALID_AMOUNT_NETWORK_DATA_NODE 34 // The amount of block verifiers that need to vote true with at least 3 of the network data nodes for the part of the round to be valid.
 #define BLOCK_VERIFIERS_VALID_AMOUNT_PERCENTAGE 0.67 // The amount of block verifiers in a percentage that need to vote true for the part of the round to be valid.
 
 
@@ -316,7 +316,7 @@ namespace config
   uint16_t const RPC_DEFAULT_PORT = 18281;
   uint16_t const ZMQ_RPC_DEFAULT_PORT = 18282;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x10 ,0x01, 0x41, 0x55 , 0x50, 0x62 , 0x41, 0x65, 0x16, 0x37, 0x00, 0x59, 0x56, 0xA5, 0x45, 0x89
+      0x10 ,0x01, 0x41, 0x55 , 0x50, 0x62 , 0x41, 0x65, 0x16, 0x37, 0x00, 0x59, 0x61, 0xA5, 0x45, 0x89
     } };
   std::string const GENESIS_TX = "013c01ff0001b197bcc5c605029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f1dde8d8d6c53e9d2e920d6e66432eaff6a85b2d25043fc29ef477b075b143df";
   uint32_t const GENESIS_NONCE = 10000;
