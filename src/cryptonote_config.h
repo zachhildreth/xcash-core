d54b5664e3c245da82c27b43ce13bf426bc948cf
zachhildreth <zachariahildreth@gmail.com>
2020-11-23 16:17:46 +0000

    test: manual test HF
    manual test HF

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -249,7 +249,7 @@ full_addrs.insert(SEED_NODE_5);
 // XCASH DPOPS
 #define BLOCK_VERIFIERS_TOTAL_AMOUNT 100 // The total amount of block verifiers
 #define BLOCK_VERIFIERS_AMOUNT 50 // The amount of block verifiers in a round
-#define BLOCK_VERIFIERS_VALID_AMOUNT 27 // The amount of block verifiers that need to vote true for the part of the round to be valid.
+#define BLOCK_VERIFIERS_VALID_AMOUNT 28 // The amount of block verifiers that need to vote true for the part of the round to be valid.
 #define BLOCK_VERIFIERS_VALID_AMOUNT_PERCENTAGE 0.55 // The amount of block verifiers in a percentage that need to vote true for the part of the round to be valid.
 
 
