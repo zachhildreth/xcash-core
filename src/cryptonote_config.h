b449d73fc05dc56e91fabb45052b8aae8410b655
zachhildreth <zachariahildreth@gmail.com>
2020-12-16 02:03:41 +0000

    test: Update xcash-dpops blockcheight
    Update xcash-dpops blockcheight.

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -218,7 +218,7 @@ full_addrs.insert(SEED_NODE_5);
 // Blockchain
 #define HF_VERSION_PROOF_OF_STAKE 13
 //#define HF_BLOCK_HEIGHT_PROOF_OF_STAKE 521850 // The first block of the X-CASH proof of stake
-#define HF_BLOCK_HEIGHT_PROOF_OF_STAKE 740000 // The first block of the X-CASH proof of stake
+#define HF_BLOCK_HEIGHT_PROOF_OF_STAKE 760000 // The first block of the X-CASH proof of stake
 #define BLOCK_TIME 5 // the block time in minutes
 #define BLOCKS_PER_DAY_FIVE_MINUTE_BLOCK_TIME 288 // The blocks per day with a 5 minute block time
 #define MAXIMUM_RESERVE_BYTES_LEGNTH 4096 // The maximum size in bytes for the reserve bytes in the block header
